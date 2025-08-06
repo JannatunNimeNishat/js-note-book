b64b26c9d753904e3dcf1fb1fc0ede9d7a2ce4b1
jannatunNimeNishat <nishatnime100@gmail.com>
2025-08-07 02:57:15 +0600

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log("2">1)// true
+// console.log(2>"1")// true
+
+console.log(null > 0) // false
+console.log(null == 0) //false
+console.log(null >= 0) // true
+
+console.log(undefined > 0) // false
+console.log(undefined == 0) //false
+console.log(undefined >= 0) // false
+
+//* strick check
+console.log("2" === 2);//false. type is also checked here. 
