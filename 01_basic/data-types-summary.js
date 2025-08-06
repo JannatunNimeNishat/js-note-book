3d26bfbde9ee9ff9784dcb1e2101e15e973c126b
jannatunNimeNishat <nishatnime100@gmail.com>
2025-08-07 03:20:09 +0600

    data-type-summary

---
+++ 01_basic/data-types-summary.js
@@ -0,0 +1,20 @@
+// Primitive
+//7 types: Number,BigInt,String Boolean, null, undefined, Symbol
+
+// Reference (Non primitive). Memory address is directly accessible
+// Array, Objects, Functions
+
+/* Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object */
