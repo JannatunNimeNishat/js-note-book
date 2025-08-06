0f70a2fbb268cc636646d61b0e8f5adeb5f62be7
jannatunNimeNishat <nishatnime100@gmail.com>
2025-08-07 01:05:50 +0600

    null vs undefined

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -1,11 +1,16 @@
 //*Primitive data types */
-//1. Number
-//2. BigInt
-//3. String
-//4. Boolean
-//5. Null
-//6. Undefined
-//7. Symbol
+//1. number => 2 to power 53
+//2. bigint
+//3. string => ""
+//4. boolean => true/false
+//5. null => standalone value
+//6. undefined
+//7. symbol
 
 console.log(typeof null); // null type is object
 console.log(typeof undefined); // undefined type is undefined
+
+/** 
+** null vs undefined
+undefined indicates the absence of a value, while null indicates the absence of an object (which could also make up an excuse for typeof null === "object"). The language usually defaults to undefined when something is devoid of a value:
+*/
